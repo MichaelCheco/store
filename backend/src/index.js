600ec5503f65b68d35b24cbfc95a1a3e050c2756
Michael <checomichael2@gmail.com>
2019-01-24 17:16:34 -0500

    restructured files/folders

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,17 +1,9 @@
-const { GraphQLServer } = require('graphql-yoga')
+const { GraphQLServer } = require('graphql-yoga');
+const { resolvers } = require('./resolvers');
 
-const typeDefs =`
-    type Query {
-        info: String!
-    }
-`
+const server = new GraphQLServer({
+  typeDefs: 'src/schema.graphql',
+  resolvers,
+});
 
-const resolvers = {
-    Query: {
-        info: () => `Testing`
-    }
-}
-
-const server = new GraphQLServer({ typeDefs, resolvers })
-
-server.start(() => console.log(`Server is running on http://localhost:4000`))
+server.start(() => console.log('Server is running on http://localhost:4000'));
---
+++ backend/src/resolvers/Query.js
@@ -0,0 +1,7 @@
+const Query = {
+  info: () => 'Testing',
+};
+
+module.exports = {
+  Query,
+};
