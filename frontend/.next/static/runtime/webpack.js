dc5d55ba9071cce0f69862bfa84a13eab8c22120
Michael <checomichael2@gmail.com>
2019-01-31 15:10:18 -0500

    changed nprogress bar color

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "08b60372c950a18fa41a";
+/******/ 	var hotCurrentHash = "6f96a354231266c8b57d";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
