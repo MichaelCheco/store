f47a9ee7e6a1144d3b5d2d58b21f62109540cf54
Michael <checomichael2@gmail.com>
2019-02-04 08:53:59 -0500

    resolver for querying all users

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -1,16 +1,27 @@
-const { forwardTo } = require('prisma-binding')
+const { forwardTo } = require("prisma-binding");
+const { hasPermission } = require("../utils");
 const Query = {
-  items: forwardTo('db'),
-  item: forwardTo('db'),
-  itemsConnection: forwardTo('db'),
+  items: forwardTo("db"),
+  item: forwardTo("db"),
+  itemsConnection: forwardTo("db"),
   me(parent, args, ctx, info) {
     // check if we have a current user ID
-    if(!ctx.request.userId)  {
+    if (!ctx.request.userId) {
       return null;
     }
-    return ctx.db.query.user({
-      where: { id: ctx.request.userId }
-    }, info)
+    return ctx.db.query.user(
+      {
+        where: { id: ctx.request.userId }
+      },
+      info
+    );
+  },
+  users: (parent, args, ctx, info) => {
+    if (!ctx.request.userId) {
+      throw new Error("You must be logged in!");
+    }
+    hasPermission(ctx.request.user, ["ADMIN", "PERMISSIONUPDATE"]);
+    return ctx.db.query.users({}, info);
   }
 };
 module.exports = Query;
