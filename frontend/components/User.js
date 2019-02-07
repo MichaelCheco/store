8cf124573c2d7d992feac640513b72857adf294b
Michael <checomichael2@gmail.com>
2019-02-07 11:04:32 -0500

    updated fiels returned from query

--- frontend/components/User.js
+++ frontend/components/User.js
@@ -2,21 +2,32 @@ import { Query } from 'react-apollo';
 import gql from 'graphql-tag';
 
 const CURRENT_USER_QUERY = gql`
-  query CURRENT_USER_QUERY {
-    me {
-      id
-      email
-      name
-      permissions
-    }
-  }
+	query CURRENT_USER_QUERY {
+		me {
+			id
+			email
+			name
+			permissions
+			cart {
+				id
+				quantity
+				item {
+					id
+					price
+					image
+					title
+					description
+				}
+			}
+		}
+	}
 `;
 
 const User = props => (
-  <Query {...props} query={CURRENT_USER_QUERY}>
-  {payload => props.children(payload)}
-  </Query>
-)
+	<Query {...props} query={CURRENT_USER_QUERY}>
+		{payload => props.children(payload)}
+	</Query>
+);
 
-export default User
+export default User;
 export { CURRENT_USER_QUERY };
