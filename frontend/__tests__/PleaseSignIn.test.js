5fb7bba7ec82ee5f988e0a531d16716d9780bff3
Michael <checomichael2@gmail.com>
2019-02-10 14:50:31 -0500

    mocks for user

---
+++ frontend/__tests__/PleaseSignIn.test.js
@@ -0,0 +1,21 @@
+import { mount } from 'enzyme';
+import toJSON from 'enzyme-to-json';
+import wait from 'waait';
+import PleaseSignIn from '../components/PleaseSignIn';
+import { CURRENT_USER_QUERY } from '../components/User';
+import { MockedProvider } from 'react-apollo/test-utils';
+import { fakeUser } from '../lib/testUtils';
+
+const notSignedInMocks = [
+	{
+		request: { query: CURRENT_USER_QUERY },
+		result: { data: { me: null } },
+	},
+];
+const signedInMocks = [
+	{
+		request: { query: CURRENT_USER_QUERY },
+		result: { data: { me: fakeUser() } },
+	},
+];
+describe('', () => {});
